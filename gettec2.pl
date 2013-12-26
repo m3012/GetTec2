4de73194749ed7e7a978591514d87d5f1a1760e8
m3012 <m30122000@yahoo.com>
2013-12-26 21:12:54 +0100

    exploring time

--- gettec2.pl
+++ gettec2.pl
@@ -1,5 +1,27 @@
-#http://zse.hr/graf_data_dionice.aspx?dionice=HT-R-A&datum=180
+#
+use strict;
+use warnings;
 
 #use Perl::Unit;
+use LWP::Simple;
+use Time::Piece;
 
-print "Hello World!\n";
+my $date = Time::Piece->new->strftime('%m/%d/%Y');
+$url = "http://zse.hr/graf_data_dionice.aspx?dionice=HT-R-A&datum=180";
+$content = get($url);
+die "Can't GET $url" if (! defined $content);
+
+print $content;
+print "\n\n";
+
+$today = Time::Piece->new->strftime('%d.%m.%Y'); 
+print 'Date: '. $today;
+
+
+my @valuesByDate = split ('--++--', $content);
+foreach (@valuesByDate) {
+	
+}
+
+
+#$value =~ $content
